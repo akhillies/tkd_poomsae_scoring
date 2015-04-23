7b5e52bc86ad199c6c4ad06288ff0ec2c373f124
Akhil Batra <akhilbatra@berkeley.edu>
2015-04-23 15:22:33 -0700

    testing updating sliders

--- index.js
+++ index.js
@@ -5,36 +5,35 @@ var updater = (function()
             var minor = 0;
             var major = 0;
 
-            function addMinor() 
-            {
-                minor += 1;
-            }
-            function subMinor()
-            {
-                major -= 1;
-            }
-            function addMajor() 
-            {
-                minor += 1;
-            }
-            function subMajor()
-            {
-                major -= 1;
-            }
-            
-            function updatePoom()
-            {
-                return 0 - 0.1 * minor - 0.3 * major;
-            }
-
-            function updatePres()
-            {
-               $(".everything #presscore").innerHTML(0);
-            }
-
-            function updateSpeed()
-            {
-                $(".everything #speednum").innerHTML($(".everything #speedin").attr("value"));
+            return {
+                addMinor: function() 
+                {
+                    minor += 1;
+                },
+                subMinor: function()
+                {
+                    major -= 1;
+                },
+                addMajor: function() 
+                {
+                    minor += 1;
+                },
+                subMajor: function()
+                {
+                    major -= 1;
+                },
+                updatePoom: function()
+                {
+                    return 0 - 0.1 * minor - 0.3 * major;
+                },
+                updatePres: function()
+                {
+                   $(".everything #presscore").innerHTML(0);
+                },
+                updateSpeed: function()
+                {
+                    $(".everything #speednum").innerHTML($(".everything #speedin").attr("value"));
+                };
             }
         })();
 
