01211f0b79772ce84e2c12f4245095d92a4c9a5d
Akhil Batra <akhilbatra@berkeley.edu>
2015-04-23 20:28:17 -0700

    quick css fix

--- yoloupdate.sh
+++ yoloupdate.sh
@@ -1,10 +1,10 @@
 git status
 git add -A
 git commit -m "$1"
-git pull
+git pull origin master
 git push origin master
 git checkout gh-pages
-git pull
+git pull origin gh-pages
 git rebase master
 git push origin gh-pages
 git checkout master
