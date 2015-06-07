572d1d8ca311346fef9b8327fd448976b54042fb
Akhil Batra <akhilbatra@berkeley.edu>
2015-06-07 01:22:04 -0700

    test

--- helperscripts/yoloupdate.sh
+++ helperscripts/yoloupdate.sh
@@ -4,7 +4,7 @@ git commit -m "$1"
 git pull origin master
 git push origin master
 git checkout gh-pages
-git pull origin gh-pages
+#git pull origin gh-pages
 git rebase master
 git push origin gh-pages
 git checkout master
