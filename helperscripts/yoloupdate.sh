3fe3fac8cb563ff8ef92985262e03150fc8d4072
Akhil Batra <akhilbatra@berkeley.edu>
2015-06-07 01:24:00 -0700

    undo test

--- helperscripts/yoloupdate.sh
+++ helperscripts/yoloupdate.sh
@@ -1,10 +1,12 @@
 git status
 git add -A
 git commit -m "$1"
-git pull origin master
-git push origin master
-git checkout gh-pages
+git pull
+git push
+#git pull origin master
+#git push origin master
+#git checkout gh-pages
 #git pull origin gh-pages
-git rebase master
-git push origin gh-pages
-git checkout master
+#git rebase master
+#git push origin gh-pages
+#git checkout master
