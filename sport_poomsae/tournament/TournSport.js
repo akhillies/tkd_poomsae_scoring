a1936ccfb3c6aecb806c42333308bb6c66f720e8
chashao <melvin94@gmail.com>
2015-06-16 16:29:16 -0700

    Continued phone scaling

--- sport_poomsae/tournament/TournSport.js
+++ sport_poomsae/tournament/TournSport.js
@@ -219,13 +219,6 @@ var updater = (function ()
 
         });
 
-//$( document ).ready(function() {      
-  //  var isMobile = window.matchMedia("only screen and (max-width: 760px)");
-
-    //if (isMobile.matches) {
-        //Conditional script here
-    //}
- //});
 
 
 
