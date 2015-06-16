56c046ae26ee652fcea380cbd4ec7e2ee4e19448
chashao <melvin94@gmail.com>
2015-06-16 16:30:47 -0700

    Revert "Continued phone scaling"
    This reverts commit a1936ccfb3c6aecb806c42333308bb6c66f720e8.

--- sport_poomsae/tournament/TournSport.js
+++ sport_poomsae/tournament/TournSport.js
@@ -219,6 +219,13 @@ var updater = (function ()
 
         });
 
+//$( document ).ready(function() {      
+  //  var isMobile = window.matchMedia("only screen and (max-width: 760px)");
+
+    //if (isMobile.matches) {
+        //Conditional script here
+    //}
+ //});
 
 
 
