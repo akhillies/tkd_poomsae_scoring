3ab5a30fefbe4609f305efa90af1c22280b2129a
Akhil Batra <akhilbatra@berkeley.edu>
2015-06-21 23:33:41 -0700

    added persistence

--- sport_poomsae/js/scoringlogic.js
+++ sport_poomsae/js/scoringlogic.js
@@ -1,34 +1,36 @@
 window.onload=$(function()
-        {
-            FastClick.attach(document.body);
-        });
+    {
+        FastClick.attach(document.body);
+    });
 
 var that = this;
 
+var scores = {
+    minor: 0,
+    major: 0,
+    spd: 0.0,
+    str: 0.0,
+    eng: 0.0,
+    presScore: 0.0,
+    accScore: 4.0,
+    totalScore: 4.0
+};
+
 var updater = (function ()
 {
-    var minor = 0;
-    var major = 0;
-    var spd = 0.0;
-    var str = 0.0;
-    var eng = 0.0;
-    var presScore = 0.0;
-    var accScore = 4.0;
-    var totalScore = 4.0;
 
     var grabValue = function(html)
     {
-        txt = html.trim()
-        var len = txt.length;
-        return txt.substring(len - 3);
-    };
+        var txt = html.trim()
+        return txt.substring(txt.length - 3);
+    }
 
     var updateMajorDisp = function(i)
     {
         //console.log("updated major disp");
-        major = Math.max(major + i, 0);
-        $("#majorcount").html(major);
-        if(major <= 0)
+        that.scores.major = Math.max(that.scores.major + i, 0);
+        $("#majorcount").html(that.scores.major);
+        if(that.scores.major <= 0)
         {
             $("#undomajorbutton").attr("disabled", true);
         }
@@ -36,15 +38,16 @@ var updater = (function ()
         {
             $("#undomajorbutton").removeAttr("disabled");
         }
+
         updateAccScore();
-    };
+    }
 
     var updateMinorDisp = function(i)
     {
         //console.log("updated minor disp")
-        minor = Math.max(minor + i, 0);
-        $("#minorcount").html(minor); 
-        if(minor <= 0)
+        that.scores.minor = Math.max(that.scores.minor + i, 0);
+        $("#minorcount").html(that.scores.minor); 
+        if(that.scores.minor <= 0)
         {
             $("#undominorbutton").attr("disabled", true);
         }
@@ -52,15 +55,16 @@ var updater = (function ()
         {
             $("#undominorbutton").removeAttr("disabled");
         }
+
         updateAccScore();
-    };
+    }
 
     var updatePres = function()
     {
-        presScore = (spd + str + eng);
-        $("#presscore").html(presScore.toFixed(1));
+        that.scores.presScore = (that.scores.spd + that.scores.str + that.scores.eng);
+        $("#presscore").html(that.scores.presScore.toFixed(1));
         updateTotal();
-        if(presScore >= 1.5 && spd >= 0.5 && str >= 0.5 && eng >= 0.5)
+        if(that.scores.presScore >= 1.5 && that.scores.spd >= 0.5 && that.scores.str >= 0.5 && that.scores.eng >= 0.5)
         {
             $("#scoresubmit").removeAttr("disabled");
         }
@@ -68,22 +72,23 @@ var updater = (function ()
         {
             $("#scoresubmit").attr("disabled", true);
         }
-    };
+    }
 
     var updateAccScore = function()
     {
         //console.log("updated acc score");
-        accScore = Math.max(0, 4 - 0.1 * minor - 0.3 * major);
-        $("#accscore").html(accScore.toFixed(1)); 
+        that.scores.accScore = Math.max(0, 4 - 0.1 * that.scores.minor - 0.3 * that.scores.major);
+        $("#accscore").html(that.scores.accScore.toFixed(1)); 
         updateTotal();
-    };
+    }
 
     var updateTotal = function()
     {
-        totalScore = presScore + accScore;
-        $("#totalscore").html(totalScore.toFixed(1));
-        $("#finalscore").html(totalScore.toFixed(1));
-    };
+        that.scores.totalScore = that.scores.presScore + that.scores.accScore;
+        $("#totalscore").html(that.scores.totalScore.toFixed(1));
+        $("#finalscore").html(that.scores.totalScore.toFixed(1));
+        localStorage.setItem('scores', JSON.stringify(that.scores));
+    }
 
     return {
         addMinor: function() 
@@ -108,63 +113,81 @@ var updater = (function ()
         },
         updateSpeed: function(dombutton)
         {
-            var id = "speed" + (spd*10).toString();
+            var id = "speed" + (that.scores.spd*10).toString();
             $("#" + id).removeClass("active");
-            spd = parseFloat(grabValue(dombutton.innerHTML));
-            $("#speednum").html(spd.toFixed(1));
+            that.scores.spd = parseFloat(grabValue(dombutton.innerHTML));
+            $("#speednum").html(that.scores.spd.toFixed(1));
             updatePres();
         },
         updateStrength: function(dombutton)
         {
-            var id = "strength" + (str*10).toString();
+            var id = "strength" + (that.scores.str*10).toString();
             $("#" + id).removeClass("active");
-            str = parseFloat(grabValue(dombutton.innerHTML));
-            $("#strengthnum").html(str.toFixed(1));
+            that.scores.str = parseFloat(grabValue(dombutton.innerHTML));
+            $("#strengthnum").html(that.scores.str.toFixed(1));
             updatePres();
         },
         updateEnergy: function(dombutton)
         {
-            var id = "energy" + (eng*10).toString();
+            var id = "energy" + (that.scores.eng*10).toString();
             $("#" + id).removeClass("active");
-            eng = parseFloat(grabValue(dombutton.innerHTML));
-            $("#energynum").html(eng.toFixed(1));
+            that.scores.eng = parseFloat(grabValue(dombutton.innerHTML));
+            $("#energynum").html(that.scores.eng.toFixed(1));
             updatePres();
         },
-
 
         updatePoomName: function(dombutton)
         {
             $("#poomsae-name").html(dombutton.innerHTML);
         },
 
+        restoreScore: function()
+        {
+            updateMajorDisp(0);
+            updateMinorDisp(0);
+
+            var id = "speed" + (that.scores.spd*10).toString();
+            $("#" + id).addClass("active");
+            $("#speednum").html(that.scores.spd.toFixed(1));
+            id = "strength" + (that.scores.str*10).toString();
+            $("#" + id).addClass("active");
+            $("#strengthnum").html(that.scores.str.toFixed(1));
+            id = "energy" + (that.scores.eng*10).toString();
+            $("#" + id).addClass("active");
+            $("#energynum").html(that.scores.eng.toFixed(1));
+            updatePres();
+        },
+
         resetScore: function()
         {
-         minor = 0;
-         major = 0;
-         updateMajorDisp(0);
-         updateMinorDisp(0);
+            that.scores.minor = 0;
+            that.scores.major = 0;
+            updateMajorDisp(0);
+            updateMinorDisp(0);
 
-         var id = "speed" + (spd*10).toString();
-         $("#" + id).removeClass("active");
-         spd = 0.0;
-         $("#speednum").html(spd.toFixed(1));
-         id = "strength" + (str*10).toString();
-         $("#" + id).removeClass("active");
-         str = 0.0;
-         $("#strengthnum").html(str.toFixed(1));
-         id = "energy" + (eng*10).toString();
-         $("#" + id).removeClass("active");
-         eng = 0.0;
-         $("#energynum").html(eng.toFixed(1));
-         updatePres();
+            var id = "speed" + (that.scores.spd*10).toString();
+            $("#" + id).removeClass("active");
+            that.scores.spd = 0.0;
+            $("#speednum").html(that.scores.spd.toFixed(1));
+            id = "strength" + (that.scores.str*10).toString();
+            $("#" + id).removeClass("active");
+            that.scores.str = 0.0;
+            $("#strengthnum").html(that.scores.str.toFixed(1));
+            id = "energy" + (that.scores.eng*10).toString();
+            $("#" + id).removeClass("active");
+            that.scores.eng = 0.0;
+            $("#energynum").html(that.scores.eng.toFixed(1));
+            updatePres();
+            
+            localStorage.setItem('scores', JSON.stringify(that.scores));
 
-         $("#submitscore").modal("hide");
+            $("#submitscore").modal("hide");
 
             //location.reload();
         },
         updateSubmit: function()
         {
-            if(presScore < 1.5 || spd < 0.5 || str < 0.5 || eng < 0.5)
+            if(that.scores.presScore < 1.5 || that.scores.spd < 0.5 || that.scores.str < 0.5 || that.scores.eng < 0.5)
             {
                 alert("Please enter a valid Presentation score.");
             }
@@ -175,6 +198,16 @@ var updater = (function ()
 
 $(document).ready(function()
 {
+    var scor = localStorage.getItem('scores');
+    if(scor)
+    {
+        that.scores = JSON.parse(scor);
+        that.updater.restoreScore();
+    }
+    else
+    {
+        localStorage.setItem('scores', JSON.stringify(that.scores));
+    }
     //provision for a window.matchMedia method here; no need for it at this moment
     
     // Enter key now removes focus from athlete-name box
@@ -195,21 +228,22 @@ $(document).ready(function()
 
     $("#scoresubmit").click(that.updater.updateSubmit);
 
+    
     $(".speedin").click(function()
-            {
-                that.updater.updateSpeed(this);
-            });
+    {
+        that.updater.updateSpeed(this);
+    });
     $(".strengthin").click(function()
-            {
-                that.updater.updateStrength(this);
-            });
+    {
+        that.updater.updateStrength(this);
+    });
     $(".energyin").click(function()
-            {
-                that.updater.updateEnergy(this);
-            });
+    {
+        that.updater.updateEnergy(this);
+    });
 
     $(".poom-names").click(function()
-            {
-                that.updater.updatePoomName(this);
-            });
+    {
+        that.updater.updatePoomName(this);
+    });
 });
