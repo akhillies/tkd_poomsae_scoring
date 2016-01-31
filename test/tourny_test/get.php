43d4d80ac583a0742dcf823431d93c7e682e44a7
Akhil Batra <akhilbatra@berkeley.edu>
2016-01-30 18:53:43 -0800

    get.php works

--- test/tourny_test/get.php
+++ test/tourny_test/get.php
@@ -1,14 +1,33 @@
+/* gets the row from datatable with the player's given ID */
+/* INPUT: player_id = id of the player you want */
+/* OUTPUT: string row with all the values comma-separated */
+
 <?php
     header('Access-Control-Allow-Origin: *');
-    $jsonStr = file_get_contents('players.json');
-    $data = json_decode($jsonStr);
 
-    if($_POST['player_id'])
+    $user = 'root';
+    $password = 'root';
+    $db = 'ucmap_tkd_scoring';
+    $host = '127.0.0.1';
+    $port = '8889';
+
+    $link = mysqli_init();
+    $success = mysqli_real_connect(
+        $link, 
+        $host,
+        $user, 
+        $password, 
+        $db,
+        $port
+    );
+
+    $sql = "SELECT * FROM sport_poomsae WHERE Player_Id={$_POST['player_id']}";
+
+
+    if ($result=mysqli_query($link,$sql))
     {
-        echo $data[$_POST['player_id']];
-    }
-    else
-    {
-        echo $data;
+        $row = $result->fetch_row();
+        echo implode(", ", $row);
     }
 ?>
+
--- test/tourny_test/sql.php
+++ test/tourny_test/sql.php
@@ -1,3 +1,7 @@
+/* gets the row from datatable with the player's given ID */
+/* INPUT: player_id = id of the player you want */
+/* OUTPUT: string row with all the values comma-separated */
+
 <?php
     header('Access-Control-Allow-Origin: *');
 
@@ -17,12 +21,14 @@
         $port
     );
 
-    $sql = "UPDATE sport_poomsae SET Ring=0";
+    $sql = "SELECT * FROM sport_poomsae WHERE Player_Id={$_POST['player_id']}";
 
-    if (mysqli_query($link,$sql))
+
+    if ($result=mysqli_query($link,$sql))
     {
-        echo "SUCESS";
-  }
-
-
+        $row = $result->fetch_row();
+        echo implode(", ", $row);
+    }
 ?>
+
+
