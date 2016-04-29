dacd15728d7fda46c6b1ad9836d184043fabc9f0
Akhil Batra <akhilbatra@berkeley.edu>
2016-04-29 10:08:42 -0700

    fixed to allow apostrophes

--- php_files/add.php
+++ php_files/add.php
@@ -12,16 +12,16 @@
         exit();
     }
 
-    $first_name = (array_key_exists('first_name', $_POST) ? $_POST['first_name'] : 'unknown');
-    $last_name = (array_key_exists('last_name', $_POST) ? $_POST['last_name'] : "unknown");
-    $middle_name = (array_key_exists('middle_name', $_POST) ? $_POST['middle_name'] : '');
-    $id = (array_key_exists('id', $_POST) ? $_POST['id'] : 0);
-    $age = (array_key_exists('age', $_POST) ? $_POST['age'] : 0);
-    $belt = (array_key_exists('belt', $_POST) ? $_POST['belt'] : "black");
-    $division = (array_key_exists('division', $_POST) ? $_POST['division'] : 0);
-    $round = (array_key_exists('round', $_POST) ? $_POST['round'] : 0);
-    $gender = (array_key_exists('gender', $_POST) ? $_POST['gender'] : 0);
-    $school = (array_key_exists('school', $_POST) ? $_POST['school'] : "unknown");
+    $first_name = mysql_real_escape_string(array_key_exists('first_name', $_POST) ? $_POST['first_name'] : 'unknown');
+    $last_name = mysql_real_escape_string(array_key_exists('last_name', $_POST) ? $_POST['last_name'] : "unknown");
+    $middle_name = mysql_real_escape_string(array_key_exists('middle_name', $_POST) ? $_POST['middle_name'] : '');
+    $id = mysql_real_escape_string(array_key_exists('id', $_POST) ? $_POST['id'] : 0);
+    $age = mysql_real_escape_string(array_key_exists('age', $_POST) ? $_POST['age'] : 0);
+    $belt = mysql_real_escape_string(array_key_exists('belt', $_POST) ? $_POST['belt'] : "black");
+    $division = mysql_real_escape_string(array_key_exists('division', $_POST) ? $_POST['division'] : 0);
+    $round = mysql_real_escape_string(array_key_exists('round', $_POST) ? $_POST['round'] : 0);
+    $gender = mysql_real_escape_string(array_key_exists('gender', $_POST) ? $_POST['gender'] : 0);
+    $school = mysql_real_escape_string(array_key_exists('school', $_POST) ? $_POST['school'] : "unknown");
         
     $sql = "INSERT INTO competitors (fname, lname, mname, id, age, belt, division, gender, round, school) VALUES ('$first_name', '$last_name', '$middle_name', $id, $age, '$belt', '$division', $gender, '$round', '$school')";
     if (mysqli_query($link,$sql))
