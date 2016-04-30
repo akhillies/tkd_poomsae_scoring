ea1568b6accfaa49553aef717b40e67be3cd00da
Akhil Batra <akhilbatra@berkeley.edu>
2016-04-29 21:58:33 -0700

    ugh

--- php_files/viewFinalScores.php
+++ php_files/viewFinalScores.php
@@ -26,61 +26,68 @@
             $i += 1;
         }
 
-        $idList = [];
-        $index = -1;
-        foreach ($scores as $scr) {
-            if($scoringInfo[$index]['id'] != $scr['id']) {
-                $index += 1;
-                $scoringInfo[$index]['id'] = $scr['id'];
-                $scoringInfo[$index]['gender'] = $scr['gender'];
-                $scoringInfo[$index]['division'] = $scr['division'];
-                $scoringInfo[$index]['round'] = $scr['round'];
-                $scoringInfo[$index]['poomsae'][$scr['poomsae']]['min'] = 10;
-                $scoringInfo[$index]['poomsae'][$scr['poomsae']]['max'] = 0;
+        if($scores)
+        {
+            $idList = [];
+            $index = -1;
+            foreach ($scores as $scr) {
+                if($scoringInfo[$index]['id'] != $scr['id']) {
+                    $index += 1;
+                    $scoringInfo[$index]['id'] = $scr['id'];
+                    $scoringInfo[$index]['gender'] = $scr['gender'];
+                    $scoringInfo[$index]['division'] = $scr['division'];
+                    $scoringInfo[$index]['round'] = $scr['round'];
+                    $scoringInfo[$index]['poomsae'][$scr['poomsae']]['min'] = 10;
+                    $scoringInfo[$index]['poomsae'][$scr['poomsae']]['max'] = 0;
+                    
+                    $idList[$index] = $scr['id'];
+                }
+                if($scr['poomsae'] > count($scoringInfo[$index]['poomsae'])) {
+                    $scoringInfo[$index]['poomsae'][$scr['poomsae']]['min'] = 10;
+                    $scoringInfo[$index]['poomsae'][$scr['poomsae']]['max'] = 0;
+                }
                 
-                $idList[$index] = $scr['id'];
+                $s = floatval($scr['score']);
+                $scoringInfo[$index]['poomsae'][$scr['poomsae']]['tscore'] += $s;
+                $min = $scoringInfo[$index]['poomsae'][$scr['poomsae']]['min'];
+                $min = $min > $s ? $s : $min;
+                $scoringInfo[$index]['poomsae'][$scr['poomsae']]['min'] = $min;
+                $max = $scoringInfo[$index]['poomsae'][$scr['poomsae']]['max'];
+                $max = $max < $s ? $s : $max;
+                $scoringInfo[$index]['poomsae'][$scr['poomsae']]['max'] = $max;
+                $scoringInfo[$index]['poomsae'][$scr['poomsae']]['judges'] += 1;
+                
+                if($scoringInfo[$index]['poomsae'][$scr['poomsae']]['judges'] >= 5) { 
+                    $scoringInfo[$index]['poomsae'][$scr['poomsae']]['fscore'] = ($scoringInfo[$index]['poomsae'][$scr['poomsae']]['tscore'] - $min - $max) / ($scoringInfo[$index]['poomsae'][$scr['poomsae']]['judges'] - 2);
+                } else {
+                    $scoringInfo[$index]['poomsae'][$scr['poomsae']]['fscore'] = $scoringInfo[$index]['poomsae'][$scr['poomsae']]['tscore'] / $scoringInfo[$index]['poomsae'][$scr['poomsae']]['judges'];
+                }
+                $scoringInfo[$index]['tfscore'] = 0;
+                for($i = 1; $i <= $scr['poomsae']; $i++) {
+                    $scoringInfo[$index]['tfscore'] += $scoringInfo[$index]['poomsae'][$scr['poomsae']]['fscore'];
+                }
+                $scoringInfo[$index]['tfscore'] /= 2;
             }
-            if($scr['poomsae'] > count($scoringInfo[$index]['poomsae'])) {
-                $scoringInfo[$index]['poomsae'][$scr['poomsae']]['min'] = 10;
-                $scoringInfo[$index]['poomsae'][$scr['poomsae']]['max'] = 0;
-            }
-            
-            $s = floatval($scr['score']);
-            $scoringInfo[$index]['poomsae'][$scr['poomsae']]['tscore'] += $s;
-            $min = $scoringInfo[$index]['poomsae'][$scr['poomsae']]['min'];
-            $min = $min > $s ? $s : $min;
-            $scoringInfo[$index]['poomsae'][$scr['poomsae']]['min'] = $min;
-            $max = $scoringInfo[$index]['poomsae'][$scr['poomsae']]['max'];
-            $max = $max < $s ? $s : $max;
-            $scoringInfo[$index]['poomsae'][$scr['poomsae']]['max'] = $max;
-            $scoringInfo[$index]['poomsae'][$scr['poomsae']]['judges'] += 1;
-            
-            if($scoringInfo[$index]['poomsae'][$scr['poomsae']]['judges'] >= 5) { 
-                $scoringInfo[$index]['poomsae'][$scr['poomsae']]['fscore'] = ($scoringInfo[$index]['poomsae'][$scr['poomsae']]['tscore'] - $min - $max) / ($scoringInfo[$index]['poomsae'][$scr['poomsae']]['judges'] - 2);
-            } else {
-                $scoringInfo[$index]['poomsae'][$scr['poomsae']]['fscore'] = $scoringInfo[$index]['poomsae'][$scr['poomsae']]['tscore'] / $scoringInfo[$index]['poomsae'][$scr['poomsae']]['judges'];
-            }
-            $scoringInfo[$index]['tfscore'] = 0;
-            for($i = 1; $i <= $scr['poomsae']; $i++) {
-                $scoringInfo[$index]['tfscore'] += $scoringInfo[$index]['poomsae'][$scr['poomsae']]['fscore'];
-            }
-            $scoringInfo[$index]['tfscore'] /= 2;
-        }
 
-        $index = 0;
-        $sql2 = "SELECT fname, lname FROM competitors WHERE id IN (" . implode(",", $idList) . ")";
-        if ($result2=mysqli_query($link,$sql2)) {
-            while ($row2 = $result2->fetch_assoc()) {
-                $scoringInfo[$index]['name'] = $row2['fname'] . " " . $row2['lname'];
-                $index += 1;
+            $index = 0;
+            $sql2 = "SELECT fname, lname FROM competitors WHERE id IN (" . implode(",", $idList) . ")";
+            if ($result2=mysqli_query($link,$sql2)) {
+                while ($row2 = $result2->fetch_assoc()) {
+                    $scoringInfo[$index]['name'] = $row2['fname'] . " " . $row2['lname'];
+                    $index += 1;
+                }
             }
-        }
 
-        usort($scoringInfo, "cmpfscore");
-        
-        $response_array['status'] = 'success'; 
-        $response_array['message'] = "YAY :D";        
-        $response_array['info'] = $scoringInfo;
+            usort($scoringInfo, "cmpfscore");
+            
+            $response_array['status'] = 'success'; 
+            $response_array['message'] = "YAY :D";        
+            $response_array['info'] = $scoringInfo;
+        } else {
+            $response_array['status'] = 'success'; 
+            $response_array['message'] = "No Scores";        
+            $response_array['info'] = [];
+        }
     } else {
         $response_array['status'] = 'failed';  
         $response_array['message'] = "Unable to get scores - maybe no player with that id has scores?";
