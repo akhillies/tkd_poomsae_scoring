a2eef09c2d9aefd1969baef919fa0cea2c00c6e6
Akhil Batra <akhilbatra@berkeley.edu>
2016-09-21 23:33:06 -0700

    updated helper git script

--- helperscripts/gitupdate.sh
+++ helperscripts/gitupdate.sh
@@ -1,5 +1,31 @@
 git status
+
+echo -n "add everything? (y/n): "
+read add
+if [ "$add" == "n" ] 
+then
+    echo "nothing happened"
+    exit
+fi
 git add -A
+
+git status
+echo -n "commit? (y/n): "
+read commit
+if [ "$commit" == "n" ] 
+then
+    echo "added but no commit"
+    exit
+fi
 git commit -m "$1"
-git pull
+
+git status
+echo -n "pull and push? (y/n): "
+read puspul 
+if [ "$puspul" == "n" ] 
+then
+    echo "committed but no push"
+    exit
+fi
+git pull --rebase
 git push
